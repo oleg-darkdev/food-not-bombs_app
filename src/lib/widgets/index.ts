e08e3653e59a2b82ee5d0dce35372ccfe58a7b9f
Oleg_DarkDev <darkdev_brand@riseup.net>
2025-12-27 14:03:33 +0100

    fix marquee import

--- src/lib/widgets/index.ts
+++ src/lib/widgets/index.ts
@@ -2,7 +2,7 @@ import LangSwitcher from './langSwitcher/ui/LangSwitcher.svelte';
 import FAQ from './faq/ui/Faq.svelte';
 import Team from './team/ui/Team.svelte';
 import VeganMenu from './recipes/ui/VeganMenu.svelte';
-import Marquee from './Marquee/ui/Marquee.svelte';
+import Marquee from './marquee/ui/Marquee.svelte';
 import BigLightDivider from './lightDividers/ui/BigLightDivider.svelte';
 import { Gallery, landingPromoPhotos } from './gallery/';
 
