62e103544d3a44f49141d77ccea42af0f3b9f707
Oleg_DarkDev <darkdev_brand@riseup.net>
2025-12-28 01:48:15 +0100

    init

---
+++ src/lib/widgets/GroupsListingMini.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { MiniFnbGroupCard } from '$entities';
+
+	let { groupsData } = $props();
+</script>
+
+<div class="mx-auto grid max-w-6xl grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-2 px-2">
+	{#each groupsData as group (group.id)}
+		{#if group.imgCollected}
+			<!-- <FnbGroupCard {group} /> -->
+			<MiniFnbGroupCard {group} />
+		{/if}
+	{/each}
+</div>
+
+<style lang="postcss"></style>
