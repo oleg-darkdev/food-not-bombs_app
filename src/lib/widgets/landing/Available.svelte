885bce472ee3830e611c2ec38aabb7e79712ddb4
Oleg_DarkDev <darkdev_brand@riseup.net>
2025-12-28 01:50:04 +0100

    refactoring

--- src/lib/widgets/landing/Available.svelte
+++ src/lib/widgets/landing/Available.svelte
@@ -1,76 +1,37 @@
 <script lang="ts">
 	// import {  } from '$shared';
 	// import {  } from '$widgets';
-	// import {  } from '$entities'
+	import { BigLinkBtn } from '$entities';
 </script>
 
 <section class="section project-page-explore">
 	<a name="print-and-play"></a>
 
 	<div class="wrap__project-page-explore-heading">
-		<h1 class="h1_semibold project-page-explore">Explore more.</h1>
+		<h2 class="h1_semibold project-page-explore">
+			Boardgame + <span class="text-orange">Technologies.</span>
+		</h2>
 	</div>
-	<div class="grid_project-page-explore">
-		<div class="item_project-page-explore wrap_top-creative-container">
-			<a href="https://github.com/oleg-darkdev/food-not-bombs_app/tree/main/static/print-and-play" target="_blank" class="link_project-page-explore w-inline-block">
-				<h1 class="h_semi-bold top-creative">Print and Play.</h1>
-			</a>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream bottom-left"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream bottom-right"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream top-right"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream top-left"
-			/>
-		</div>
-		<div class="item_project-page-explore wrap_top-creative-container">
-			<a href="/app" target="_blank" class="link_project-page-explore w-inline-block">
-				<h1 class="h_semi-bold top-creative">Web Application</h1>
-			</a>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream bottom-left"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream bottom-right"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream top-right"
-			/>
-			<img
-				src="/images/corner-dot-white.svg"
-				loading="lazy"
-				alt=""
-				class="svg_corner-dot-cream top-left"
-			/>
-		</div>
+	<div class="mx-auto mb-4 flex max-w-6xl flex-row justify-between">
+		<BigLinkBtn
+			text="Print and Play."
+			bgColor="bg-orange"
+			link="https://github.com/oleg-darkdev/food-not-bombs_app/tree/main/static/print-and-play"
+		/>
+		<BigLinkBtn text="Web Application" bgColor="bg-red" link="/app" />
+	</div>
+	<div class="mx-auto flex max-w-6xl flex-row justify-between">
+		<BigLinkBtn text="Public API" link="/api" bgColor="bg-red" />
+		<BigLinkBtn text="FNB Groups list" link="/groups" bgColor="bg-orange" />
 	</div>
 </section>
 
 <style lang="postcss">
+	.grid_project-page-explore {
+		/* grid-column-gap: 1em;
+	grid-row-gap: 1em;
+	justify-content: center;
+	align-items: center;
+	display: flex; */
+	}
 </style>
