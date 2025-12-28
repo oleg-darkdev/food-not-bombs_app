89957d320bab32a2785e2cbde0ed61d974fe7672
Oleg_DarkDev <darkdev_brand@riseup.net>
2025-12-28 01:48:38 +0100

    refactoring recipes

--- src/lib/widgets/recipes/ui/VeganMenu.svelte
+++ src/lib/widgets/recipes/ui/VeganMenu.svelte
@@ -1,51 +1,37 @@
 <script lang="ts">
 	import recipes from '../data/recipes';
 	// import {  } from '$widgets';
-	// import {  } from '$entities'
+	import { RecipeCard } from '$entities'
 
-	const recipesPromo = [
-		recipes[0],
-		recipes[3],
-		recipes[5],
-	]
-	
+	const recipesPromo = [recipes[0], recipes[3], recipes[5]];
 </script>
 
 <section class="section services">
 	<div class="wrap_section-paragraph">
-		<h2 class="h_regular section-paragraph">
-			Готовим исключительно вегетаринскую еду: <span class="text-span-3">60 литров супа </span>
-			<em class="italic-text-2">10 кг салата </em>
-			<em class="italic-text">15 литров фруктового компота</em>.
+		<h2 class="h_regular section-paragraph mx-auto max-w-4xl">
+			<!-- Каждый раунд игры игроки стараются <span class="text-span-3"
+				>спасти как можно больше еды перед утилизацией, 
+			</span> и готовят исключительно <span class="text-span-3">вегетаринскую еду</span> для нуждающихся:
+
+			<span class="flex flex-col">
+				<em class="italic-text">60 литров супа </em>
+				<em class="italic-text-2">10 кг салата </em>
+				<em class="italic-text">15 литров компота</em>
+			</span> -->
+			Each round of the game, players try to
+			<span class="text-span-3"> save as much food as possible from disposal, </span>
+			and prepare exclusively <span class="text-span-3">vegetarian food</span> for those in need:
+
+			<span class="flex flex-col">
+				<em class="italic-text">60 liters of soup</em>
+				<em class="italic-text-2">10 kg of salad</em>
+				<em class="italic-text">15 liters of compote</em>
+			</span>
 		</h2>
 	</div>
 	<div class="wrap_services-cards">
 		{#each recipesPromo as recipe}
-			<div class="wrap_card-style-2 active-shadow">
-				<div class="wrap_card-content services">
-					<div class="wrap_card-heading services">
-						<h3 class="h_semi-bold services italic">{recipe.title}</h3>
-					</div>
-					<div class="wrap_services-list">
-						<ul role="list" class="list_services-card w-list-unstyled">
-							{#each recipe.ingredients as ingredient}
-								<li class="item_services-card">
-									<div class="txt_services-list-item caps service">
-										{ingredient.title}
-										<!-- {ingredient.count} -->
-									</div>
-								</li>
-							{/each}
-						</ul>
-					</div>
-					<!-- <div class="w-layout-vflex wrap_service-card-cta">
-						<a href="/" class="cta_primary service w-inline-block">
-							<div class="txt_cta-name underline">Learn more</div>
-						</a>
-					</div> -->
-				</div>
-				<div class="img_active-shadow"></div>
-			</div>
+			<RecipeCard {recipe}/>
 		{/each}
 	</div>
 	<div class="img_pink-service-band left">
